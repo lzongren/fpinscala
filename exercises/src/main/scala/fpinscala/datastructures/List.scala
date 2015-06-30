8551baf9a2bc9d99428e085be79e01a91985dcbf
lzongren <lzongren@gmail.com>
2015-06-30 16:50:52 +0200

    Finish chapter 3 exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,19 +50,128 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => xs
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(x, xs) => Cons(h, xs)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = n match {
+    case 0 => l
+    case neg if (neg < 0) => l
+    case _ => l match {
+      case Nil => Nil
+      case Cons(x, xs) => drop(xs, n - 1)
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) =>
+      if (f(x)) dropWhile(xs, f)
+      else xs
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)( (a, b) => b + 1 )
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  /** Exercise 7 */
+  // No, because foldRight will traverse down to bottom first then calculate.
+
+  /** Exercise 8 */
+  foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _))
+
+  /** Exercise 11 */
+  def sum3(ns: List[Int]): Int = foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]): Double = foldLeft(ns, 1: Double)(_ * _)
+
+  def length3[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
+
+  /** Exercise 12 */
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])( (b, a) => Cons(a, b) )
+
+  /** Exercise 13 */
+  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+
+  /** Exercise 14 */
+  def append1(l: List[Int], e: Int): List[Int] = foldRight(l, Cons(e, Nil))(Cons(_, _))
+
+
+  /** Exercise 15 */
+  def flatten[A](ls: List[List[A]]): List[A] = ls match {
+    case Nil => Nil
+    case Cons(x, xs) => foldLeft(xs, x)((b, a) => append(b, a))
+  }
+
+  /** Exercise 16 */
+  def addOne(l: List[Int]): List[Int] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => Cons(x + 1, addOne(xs))
+  }
+
+  /** Exercise 17 */
+  def toString(l: List[Double]): List[String] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => Cons(x.toString, toString(xs))
+  }
+
+
+  /** Exercise 18 */
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => Cons(f(x), map(xs)(f))
+  }
+
+
+  /** Exercise 19 */
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = foldLeft(l, Nil: List[A]) {
+    (b, a) =>
+      if (f(a)) Cons(a, b)
+      else b
+  }
+
+  /** Exercise 20 */
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = foldLeft(l, Nil: List[B]) {
+    (b, a) => append(b, f(a))
+  }
+
+  /** Exercise 21 */
+  def filter2[A](l: List[A])(f: A => Boolean): List[A] = flatMap(l) {
+    (a: A) =>
+      if (f(a)) Cons(a, Nil)
+      else Nil
+  }
+
+  /** Exercise 22 */
+  def addLists(l1: List[Int], l2: List[Int]): List[Int] = (l1, l2) match {
+    case (_, Nil) => l1
+    case (Nil, _) => l2
+    case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, addLists(xs, ys))
+  }
+
+  /** Exercise 23 */
+  def pairWise[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = (l1, l2) match {
+    case (_, Nil) => l1
+    case (Nil, _) => l2
+    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), pairWise(xs, ys)(f))
+  }
+
+
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,34 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  /** Exercise 25 */
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(v) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
+  /** Exercise 26 */
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max (maximum(r))
+  }
 
+  /** Exercise 27 */
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(v) => 1
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  /** Exercise 28 */
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  /** Exercise 29 */
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
 
 }
