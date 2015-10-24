c08819132b34b4a6036f67c7cbb37b96ef51d3d5
lzongren <lzongren@gmail.com>
2015-10-24 22:08:54 +0200

    Update chapter 10 solutions

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -20,17 +20,51 @@ object Monoid {
     val zero = Nil
   }
 
-  val intAddition: Monoid[Int] = sys.error("todo")
+  /** Exercise 1 */
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 + a2
+    def zero: Int = 0
+  }
 
-  val intMultiplication: Monoid[Int] = sys.error("todo")
+  /** Exercise 1 */
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 * a2
+    def zero: Int = 1
+  }
 
-  val booleanOr: Monoid[Boolean] = sys.error("todo")
+  /** Exercise 1 */
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
 
-  val booleanAnd: Monoid[Boolean] = sys.error("todo")
+    def zero: Boolean = false
+  }
 
-  def optionMonoid[A]: Monoid[Option[A]] = sys.error("todo")
+  /** Exercise 1 */
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    def zero: Boolean = true
+  }
 
-  def endoMonoid[A]: Monoid[A => A] = sys.error("todo")
+
+  /** Exercise 2 */
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]): Option[A] = (a1, a2) match {
+      case (None, None) => None
+      case (Some, None) => a1
+      case (None, Some) => a2
+      case (Some, Some) => a1
+    }
+
+    def zero: Option[A] = None
+  }
+
+
+  /** Exercise 3 */
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[(A) => A] {
+    def op(a1: (A) => A, a2: (A) => A): (A) => A = a => a1(a2(a)) // a1.composeOf(a2)
+
+    def zero: (A) => A = a => a
+  }
 
   // TODO: Placeholder for `Prop`. Remove once you have implemented the `Prop`
   // data type from Part 2.
@@ -48,46 +82,103 @@ object Monoid {
   def concatenate[A](as: List[A], m: Monoid[A]): A =
     sys.error("todo")
 
-  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
-    sys.error("todo")
 
+  /** Exercise 5 */
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
+    as.foldRight(m.zero)( (a, acc) => m.op(f(a), acc) )
+
+
+  /** Exercise 6 */
   def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
-    sys.error("todo")
+    foldMap(as, endoMonoid[B])(f.curried)(z)
+
+
 
   def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
     sys.error("todo")
 
-  def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
-    sys.error("todo")
 
-  def ordered(ints: IndexedSeq[Int]): Boolean =
-    sys.error("todo")
+  /** Exercise 7 */
+  def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B = as match {
+    case Nil => m.zero
+    case Seq(x, Nil) => f(x)
+    case _ => {
+      val (l1, l2) = as.splitAt(as.length / 2)
+      m.op(foldMapV(l1, m)(f), foldMapV(l2, m)(f))
+    }
+  }
+
+
+  /** Exercise 8 */
+  import fpinscala.parallelism.Nonblocking._
+
+  def par[A](m: Monoid[A]): Monoid[Par[A]] = new Monoid[Par[A]] {
+    def op(a1: Par[A], a2: Par[A]): Par[A] = Par.map2(a1, a2)(m.op)
+    def zero: Par[A] = Par.unit(m.zero)
+  }
+
+  def parFoldMap[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] = v match {
+    case Nil => par(m).zero
+    case _ =>
+      val (l1, l2) = v.splitAt(v.length / 2)
+      par(m).op(parFoldMap(l1, m)(f), parFoldMap(l2, m)(f))
+  }
+
+  /** Exercise 9 */
+  def ordered(ints: IndexedSeq[Int]): Boolean = {
+    val orderedMonad: Monoid[Int] = new Monoid[Int] {
+      def op(a1: Int, a2: Int): Int =
+        if (a1 <= a2) 1
+        else 0
+
+      def zero: Int = 0
+    }
+
+    foldLeft(ints.toList)(orderedMonad.zero)(orderedMonad.op) == ints.length
+  }
 
   sealed trait WC
   case class Stub(chars: String) extends WC
   case class Part(lStub: String, words: Int, rStub: String) extends WC
 
-  def par[A](m: Monoid[A]): Monoid[Par[A]] = 
-    sys.error("todo")
+  /** Exercise 10 */
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    def op(a: WC, b: WC) = (a, b) match {
+      case (Stub(c), Stub(d)) => Stub(c + d)
+      case (Stub(c), Part(l, w, r)) => Part(c + l, w, r)
+      case (Part(l, w, r), Stub(c)) => Part(l, w, r + c)
+      case (Part(l1, w1, r1), Part(l2, w2, r2)) =>
+        Part(l1, w1 + (if ((r1 + l2).isEmpty) 0 else 1) + w2, r2)
+    }
 
-  def parFoldMap[A,B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] = 
-    sys.error("todo") 
+    def zero: WC = Stub("")
+  }
 
-  val wcMonoid: Monoid[WC] = sys.error("todo")
+  def count(s: String): Int = ???
 
-  def count(s: String): Int = sys.error("todo")
+  /** Exercise 16 */
+  def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] = new Monoid[(A, B)] {
+    def op(a1: (A, B), a2: (A, B)): (A, B) = (A.op(a1._1, a2._1), B.op(a1._2, a2._2))
+    def zero: (A, B) = (A.zero, B.zero)
+  }
 
-  def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-    sys.error("todo")
+  /** Exercise 17 */
+  def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] = new Monoid[(A) => B] {
+    def op(a1: (A) => B, a2: (A) => B): (A) => B = (A) => B.op(a1(A), a2(A))
+    def zero: (A) => B = (A) => B.zero
+  }
 
-  def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] =
-    sys.error("todo")
+  def mapMergeMonoid[K,V](V: Monoid[V]): Monoid[Map[K, V]] = new Monoid[Map[K, V]] {
+    def op(a1: Map[K, V], a2: Map[K, V]): Map[K, V] =
+      (a1.keySet ++ a2.keySet).foldLeft(zero) { (acc, k) =>
+        acc.updated(k, V.op(a1.getOrElse(k, V.zero), a2.getOrElse(k, V.zero)))
+      }
+    def zero: Map[K, V] = Map.empty
+  }
 
-  def mapMergeMonoid[K,V](V: Monoid[V]): Monoid[Map[K, V]] =
-    sys.error("todo")
-
+  /** Exercise 18 */
   def bag[A](as: IndexedSeq[A]): Map[A, Int] =
-    sys.error("todo")
+    foldMapV(as, mapMergeMonoid[A, Int](intAddition))((a: A) => Map(a -> 1))
 }
 
 trait Foldable[F[_]] {
